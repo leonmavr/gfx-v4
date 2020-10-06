167f95d82f7c4a7a4a0488de0fe06f030a97d4a1
0xleo <0xleo.git@gmail.com>
2020-10-06 03:50:11 +0200

    Add 3 spinning triangles in example.c

--- example.c
+++ example.c
@@ -16,7 +16,7 @@ int main()
 	int ysize = 320;
 	int xsize = 640;
 
-	char c;
+	//char c;
 
 	// Open a new window for drawing.
 	gfx_open(xsize,ysize,"Example Graphics Program");
@@ -24,29 +24,35 @@ int main()
 	// Set the current drawing color to green.
 	gfx_color(0,200,100);
 
-	// Draw line
+	// Draw triangles and animate 
 	Queue* q = malloc(sizeof(Queue));
 	queue_init(q);
 	gfx_color(50,200,0);
-	Point pt1 = {200, 200, 1}, pt2 = {250, 240,1}, pt3 = {190, 260, 1};
-	//gfx_triangle_fill(pt1.x, pt1.y, pt2.x, pt2.y, pt3.x, pt3.y);
-	gfx_color(200,50,0);
+	Point tr11 = {190, 200, 1}, tr12 = {200, 160, 1}, tr13 = {210,200, 1};
+	Point tr21 = {180, 200, 1}, tr22 = {200, 140, 1}, tr23 = {220, 200, 1};
+	Point tr31 = {165, 200, 1}, tr32 = {200, 100, 1}, tr33 = {235, 200, 1};
+
 	int i;
+	float deg_step = -10.0;
 
-	while(1) {
-		for (i = 0; i < 20; i++) {
-			gfx_triangle_fill(pt1.x, pt1.y, pt2.x, pt2.y, pt3.x, pt3.y);
-			geo_rotateTriangle(&pt1, &pt2, &pt3, 5);
-			gfx_flush();
-			sleep(1.0);
-			gfx_clear();
-		}
-		// Wait for the user to press a character.
-		c = gfx_wait();
-
-		// Quit if it is the letter q.
-		if(c=='q') break;
+	for (i = 0; i < 15; i++) {
+		gfx_color(50,0,200);
+		gfx_triangle_fill(tr31.x, tr31.y, tr32.x, tr32.y, tr33.x, tr33.y);
+		geo_rotateTriangle(&tr31, &tr32, &tr33, deg_step);
+		gfx_color(50,200,0);
+		gfx_triangle_fill(tr21.x, tr21.y, tr22.x, tr22.y, tr23.x, tr23.y);
+		geo_rotateTriangle(&tr21, &tr22, &tr23, 2*deg_step);
+		gfx_color(200,50,0);
+		gfx_triangle_fill(tr11.x, tr11.y, tr12.x, tr12.y, tr13.x, tr13.y);
+		geo_rotateTriangle(&tr11, &tr12, &tr13, 3*deg_step);
+		gfx_flush();
+		sleep(1);
+		gfx_clear();
 	}
+	// Wait for the user to press a character.
+	//c = gfx_wait();
+	// Quit if it is the letter q.
+	//if (c=='q') break;
 	queue_del(q);
 
 	return 0;
