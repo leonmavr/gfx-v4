940682533a88c49874bb3ff237e5503e9ead2a59
0xleo <0xleo.git@gmail.com>
2021-06-15 17:15:43 +0300

    Remove some dead code, free some pointers

--- example.c
+++ example.c
@@ -18,8 +18,6 @@ int main()
 	int ysize = 980;
 	int xsize = 1280;
 
-	//char c;
-
 	// Open a new window for drawing.
 	gfx_open(xsize,ysize,"Example Graphics Program");
 
@@ -37,7 +35,6 @@ int main()
 	queue_init(q23);
 	gfx_color(50,200,0);
 
-	//vec2i_t pt1 = {10, 20}, pt2 = {250, 20};
 	Pixel pt1 = {{300, 20}, {255, 0, 0}};
 	Pixel pt2 = {{840, 650}, {0, 255, 0}};
 	Pixel pt3 = {{100, 800}, {0, 0, 255}};
@@ -56,36 +53,22 @@ int main()
 	algos_triangle_fill_bres(&pt4, &pt5, &pt6, q45, q46, q56);
 
 	gfx_flush();
-	sleep(30);
+	sleep(5);
 	gfx_clear();
 
 	queue_del(q12);
 	queue_del(q13);
 	queue_del(q23);
+	queue_del(q45);
+	queue_del(q56);
+	queue_del(q46);
 	queue_del(q);
-#if 0
-	int i;
-	float deg_step = -10.0;
-
-	for (i = 0; i < 15; i++) {
-		gfx_color(50,0,200);
-		algos_triangle_fill_int_test(&tr31, &tr32, &tr33);
-		geo_rotateTriangle(&tr31, &tr32, &tr33, deg_step);
-		gfx_color(50,200,0);
-		algos_triangle_fill_int_test(&tr21, &tr22, &tr23);
-		geo_rotateTriangle(&tr21, &tr22, &tr23, 2*deg_step);
-		gfx_color(200,50,0);
-		algos_triangle_fill_int_test(&tr11, &tr12, &tr13);
-		geo_rotateTriangle(&tr11, &tr12, &tr13, 3*deg_step);
-		gfx_flush();
-		sleep(1);
-		gfx_clear();
-	}
-	// queue_del(q);
-	// Wait for the user to press a character.
-	//char c = gfx_wait();
-	// Quit if it is the letter q.
-	//if (c=='q') break;
-#endif
+	free(q12);
+	free(q13);
+	free(q23);
+	free(q45);
+	free(q56);
+	free(q46);
+	free(q);
 	return 0;
 }
